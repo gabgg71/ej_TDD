b56566dfefdb08b3c8c9a05294fa01c74d4bbbe5
Gab <gabrielaggarcia70@gmail.com>
2023-03-05 01:04:28 +0000

    credenciales

--- arreglo.py
+++ arreglo.py
@@ -5,7 +5,7 @@ s3 = boto3.client("s3")
 response = s3.upload_file('./arreglo.py', 'ejerciciozappaa', 'arreglo.py')
 
 def menor(arreglo):
-    print("ya")
+    print("ya esta")
     return min(arreglo)
     
 
